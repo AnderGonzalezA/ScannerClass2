715d42d8217cf028b01a562b23e51d82851b40b4
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2018-09-26 08:21:15 +0200

    Scanner Class 2

--- src/Summing.java
+++ src/Summing.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
-//import org.apache.commons.lang3.StringUtils;
 public class Summing {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n = 0;
 		int x = 0;
-		while (n<10) {
+		while (n<100) {
+			System.out.print("Enter a number please");
 			Scanner m = new Scanner(System.in);
 			x = m.nextInt();
-			//if (StringUtils.isNumericSpace(m) == true) {
-				n = n + 1;
-				if (x % 2 == 0) {
-					System.out.print("This number is even");
-				}else {
-					System.out.print("This number is odd");
-				}
-			//}else {
-				//System.out.print("Please, enter a valid number");
-			//}
+			n = n + x;
 		}
+		System.out.print("The sum of your numbers is already higher than 100");
 	}
 
 }
